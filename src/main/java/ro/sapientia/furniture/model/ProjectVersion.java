604e015a0d403dd36800b0d55aa619b75f58d27b
Fulop-Berni <fulop.bernadett@student.ms.sapientia.ro>
2026-01-04 20:42:27 +0200

    Project and ProjectVersion entities added

---
+++ src/main/java/ro/sapientia/furniture/model/ProjectVersion.java
@@ -0,0 +1,99 @@
+package ro.sapientia.furniture.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+@Entity(name = "project_version")
+public class ProjectVersion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="pk_project_version")
+    @SequenceGenerator(name="pk_project_version", sequenceName="pk_project_version")
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "version_number")
+    private int versionNumber;
+
+    @Column(name = "saved_at")
+    private LocalDateTime savedAt;
+
+    @Column(name = "version_note")
+    private String versionNote;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @OneToMany(mappedBy = "version")
+    private List<FurnitureBody> bodies = new ArrayList<>();
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getVersionNumber() {
+        return versionNumber;
+    }
+
+    public void setVersionNumber(int versionNumber) {
+        this.versionNumber = versionNumber;
+    }
+
+    public LocalDateTime getSavedAt() {
+        return savedAt;
+    }
+
+    public void setSavedAt(LocalDateTime savedAt) {
+        this.savedAt = savedAt;
+    }
+
+    public String getVersionNote() {
+        return versionNote;
+    }
+
+    public void setVersionNote(String versionNote) {
+        this.versionNote = versionNote;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public List<FurnitureBody> getBodies() {
+        return bodies;
+    }
+
+    public void setBodies(List<FurnitureBody> bodies) {
+        this.bodies = bodies;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectVersion [id=" + id + ", versionNumber=" + versionNumber + ", savedAt=" + savedAt + "]";
+    }
+}
